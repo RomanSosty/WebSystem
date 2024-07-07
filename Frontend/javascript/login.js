1cce464af586fd6958ad8ed719e0090acb7b97a3
Roman Šostok <102825558+RomanSosty@users.noreply.github.com>
2024-07-07 22:01:28 +0200

    format

--- Frontend/javascript/login.js
+++ Frontend/javascript/login.js
@@ -1,30 +1,30 @@
-document.getElementById("loginForm").addEventListener("submit", function() {
-    event.preventDefault();
+document.getElementById("loginForm").addEventListener("submit", function () {
+  event.preventDefault();
 
-    const data = {
-        username: document.getElementById("username").value,
-        password: document.getElementById("password").value
-    };
+  const data = {
+    username: document.getElementById("username").value,
+    password: document.getElementById("password").value,
+  };
 
-    fetch("http://localhost:8080/authAndGenerateToken", {
-        method: "POST",
-        headers: {
-            "Content-Type": "application/json"
-        },
-        body: JSON.stringify(data)
+  fetch("http://localhost:8080/authAndGenerateToken", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(data),
+  })
+    .then(async (response) => {
+      if (!response.ok) {
+        throw Error(response.statusText);
+      }
+      localStorage.setItem("JWT", await response.text());
     })
-        .then(async response => {
-            if (!response.ok) {
-                throw Error(response.statusText);
-            }
-            localStorage.setItem("JWT", await response.text());
-        })
-        .then(data => {
-            console.log("Success:",data);
-            window.location.href = "../main.html";
-        })
-        .catch((error) => {
-            console.log(data);
-            console.log("Error:", error);
-        })
-})
+    .then((data) => {
+      console.log("Success:", data);
+      window.location.href = "../main.html";
+    })
+    .catch((error) => {
+      console.log(data);
+      console.log("Error:", error);
+    });
+});
